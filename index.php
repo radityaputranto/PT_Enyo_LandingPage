c9213369ab909fdcfc57e90de411d18a80707050
radityaputranto <radityaputranto97@yahoo.co.id>
2017-09-02 18:32:17 +0700

    layout header

---
+++ index.php
@@ -0,0 +1,178 @@
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="favicon.ico">
+
+    <title>Cover Template for Bootstrap</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="general/normalize.css">
+    <link rel="stylesheet" type="text/css" href="general/style.css">
+
+    <!-- Custom styles for this template -->
+      <!-- <link href="css/cover.css" rel="stylesheet"> -->
+    </head>
+  <body>
+
+<!-- header -->
+    <nav class="navbar navbar-expand-lg navbar-light bg-faded">
+      <a class="navbar-brand" href="#">HijabDien</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item active">
+            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Features</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Pricing</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link disabled" href="#">Disabled</a>
+          </li>
+          </ul>
+        </div>
+      </nav>
+
+    <div class="banner-img">
+            
+            <div class="  container"> 
+                  <h1>Enyo Company</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eros elit, ultricies non tortor id, posuere varius sapien. Donec arcu enim, ullamcorper sed risus eu, mollis feugiat lorem. Quisque a egestas magna. Phasellus scelerisque hendrerit mauris,</p>
+                    <button>Download</button>
+
+            </div>
+    </div>
+    
+
+    
+  
+<!-- end-header -->
+
+<!-- download it -->
+    <div class="container relative">
+      <div class="row">
+
+          <div class="col-xs-12 d-md-none"> 
+              <img class="img-fluid "src="http://placehold.it/855x440">
+          </div>
+
+          <div class="col-xs-12 col-sm-12 col-md-7"> 
+            
+              <h2>JUST DOWNLOAD IT</h2>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eros elit, ultricies non tortor id, posuere varius sapien. Donec arcu enim, ullamcorper sed risus eu, mollis feugiat lorem. Quisque a egestas magna. Phasellus scelerisque hendrerit mauris, ac ullamcorper felis fermentum vitae. Nulla facilisis mi at </p>
+
+              <ul>
+                <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit. </li>
+                <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit. </li>
+                <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit. </li>
+              </ul>
+          </div>
+
+           <div class="col-xs-12 d-none  d-md-block col-md-5"> 
+              <img class="img-fluid "src="http://placehold.it/855x440">
+          </div>
+        
+      </div>
+      
+    </div>
+<!-- end download it -->
+    
+    <div class="container">
+      <div class="row">    
+      
+        <div class="col-xs-12 col-sm-6 col-lg-4">
+          <div class="media">
+            <img class="d-flex mr-3" src="http://placehold.it/50x50" alt="Generic placeholder image">
+            <div class="media-body">
+                <h5 class="mt-0">Media heading</h5>
+                Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+            </div>
+          </div>
+        </div>
+
+        <div class="col-xs-12 col-sm-6 col-lg-4">
+          <div class="media">
+            <img class="d-flex mr-3" src="http://placehold.it/50x50" alt="Generic placeholder image">
+            <div class="media-body">
+                <h5 class="mt-0">Media heading</h5>
+                Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+            </div>
+          </div>
+        </div>
+
+        <div class="col-xs-12 col-sm-6 col-lg-4">
+          <div class="media">
+            <img class="d-flex mr-3" src="http://placehold.it/50x50" alt="Generic placeholder image">
+            <div class="media-body">
+                <h5 class="mt-0">Media heading</h5>
+                Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+            </div>
+          </div>
+        </div>
+
+        <div class="col-xs-12 col-sm-6 col-lg-4">
+          <div class="media">
+            <img class="d-flex mr-3" src="http://placehold.it/50x50" alt="Generic placeholder image">
+            <div class="media-body">
+                <h5 class="mt-0">Media heading</h5>
+                Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+            </div>
+          </div>
+        </div>
+
+        <div class="col-xs-12 col-sm-6 col-lg-4">
+          <div class="media">
+            <img class="d-flex mr-3" src="http://placehold.it/50x50" alt="Generic placeholder image">
+            <div class="media-body">
+                <h5 class="mt-0">Media heading</h5>
+                Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+            </div>
+          </div>
+        </div>
+
+        <div class="col-xs-12 col-sm-6 col-lg-4">
+          <div class="media">
+            <img class="d-flex mr-3" src="http://placehold.it/50x50" alt="Generic placeholder image">
+            <div class="media-body">
+                <h5 class="mt-0">Media heading</h5>
+                Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+            </div>
+          </div>
+        </div>
+
+
+
+      </div>
+    </div>
+
+
+<!-- content -->
+
+
+
+
+
+
+<!-- end-content -->
+
+<!-- footer -->
+<!-- end-footer -->
+
+<!-- javascript -->
+	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
+   <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js"></script>
+	<script type="text/javascript" src="js/bootstrap.js"></script>
+
+
+</body>
+</html>
